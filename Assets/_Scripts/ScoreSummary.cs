b4c1d56a7ff0a03ac42927692e7fad8d81c0b091
Devon Gardner <devon.james.gardner@gmail.com>
2016-05-08 17:37:29 -0400

    intro and summary

---
+++ Assets/_Scripts/ScoreSummary.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreSummary : MonoBehaviour {
+
+	public GameObject summaryObj;
+	Text summary;
+	// Use this for initialization
+	void Awake () {
+		summary = summaryObj.GetComponent<Text>();
+	}
+
+	void Start () {
+		summary.text = "People Rescued: " + GameManager.i.peopleSaved + "\nCanisters Used: " + GameManager.i.canistersUsed + "\nTime Spent: " + GameManager.i.timeSpent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
